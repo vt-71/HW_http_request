e3f2401d47e72d7230e0715f4f31a22e13a006bd
Vladimir Tyan <tyanv01@gmail.com>
2022-08-10 19:58:09 +0300

    Добавлена функция записи на ЯД

--- Task_2 file to Yandex.py
+++ Task_2 file to Yandex.py
@@ -9,7 +9,6 @@ class YandexDisk:
         self.token = token
 
     def get_headers(self):
-        # print(self.__dict__)
         return {
                 'Content-Type': 'application/json',
                 'Authorization': 'OAuth {}'.format(self.token)
@@ -21,9 +20,26 @@ class YandexDisk:
     
         response = requests.get(files_url, headers=headers)
         return response.json()
+    
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        # response_href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response_href = self._get_upload_link(disk_file_path=disk_file_path)
+        url = response_href.get("href", "")
+        # href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(url, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
 
 if __name__ =="__main__":
 
     ya = YandexDisk(token = TOKEN)
-    # print(ya.__dict__)
-    pprint(ya.get_files_list())
+    ya.upload_file_to_disk(disk_file_path='/text.txt', filename = 'text.txt')
